003797f37f1e93b6a204a7cba3b36e5df26bd4bf
Nueramarcos <Marcosrodriguez0324@gmail.com>
2026-06-18 12:44:35 -0700

    refactor: export Orion public API from package __init__
    - Explicit imports from .ast_parser with __all__
    - Tests import from Orion package instead of ast_parser directly

--- Orion/__init__.py
+++ Orion/__init__.py
@@ -1,12 +1,3 @@
-"""
-Public API exports for Orion.
-"""
-
 from .ast_parser import parse_file, parse_source, ProjectAnalyser, main
 
-__all__ = [
-    "parse_file",
-    "parse_source",
-    "ProjectAnalyser",
-    "main"
-]
+__all__ = ['parse_file', 'parse_source', 'ProjectAnalyser', 'main']
